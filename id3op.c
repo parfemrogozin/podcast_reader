333c231810a4fc477f385d96032694baa99607c6
Parfem Rogozin <parfem.rogozin@gmail.com>
2022-05-04 11:39:52 +0200

    id3tag truncate functions

--- id3op.c
+++ id3op.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 
 size_t decode7bit(unsigned char four_bytes[])
@@ -20,10 +22,10 @@ size_t decode7bit(unsigned char four_bytes[])
 int remove_id3v2(char * filename)
 {
   int ret_code;
-  FILE *mp3file;
   char first_bytes[3];
-
+  FILE *mp3file;
   mp3file = fopen(filename, "r");
+
   fread(first_bytes, 1, 3, mp3file);
 
   if (first_bytes[0] == 'I' && first_bytes[1] == 'D' && first_bytes[2] == '3')
@@ -45,22 +47,61 @@ int remove_id3v2(char * filename)
       fwrite(read_buffer, BUFSIZ, 1, cutfile);
     }
     fclose(cutfile);
+    fclose(mp3file);
     rename(temp_filename, filename);
     ret_code = 0;
   }
   else
   {
+    fclose(mp3file);
     fprintf(stderr, "ID3v2 tag header not found. \n");
     ret_code = 1;
   }
-
-  fclose(mp3file);
   return ret_code;
+}
+
+off_t get_offset_id3v1(char * mp3filename)
+{
+  int ret_value;
+  char tag_buffer[4];
+  off_t position;
+  FILE *mp3file;
+  mp3file = fopen(mp3filename, "r");
+  memset(tag_buffer, 0, sizeof(tag_buffer));
+  fseek(mp3file, -128, SEEK_END);
+  position = ftello(mp3file);
+  fread(tag_buffer, 1, 3, mp3file);
+  if (tag_buffer[0] == 'T' && tag_buffer[1] == 'A' && tag_buffer[2] == 'G')
+  {
+    ret_value = position;
+  }
+  else
+  {
+    ret_value = 0;
+  }
+  return ret_value;
+}
+
+void remove_id3v1(char * mp3filename)
+{
+  off_t offset;
+  offset = get_offset_id3v1(mp3filename);
+  if (offset > 0)
+  {
+    truncate(mp3filename, offset);
+  }
+  else
+  {
+    fprintf(stderr, "ID3v1 tag header not found. \n");
+  }
+
 }
 
 int main(int argc, char **argv)
 {
-  remove_id3v2(argv[1]);
+  char * mp3filename = argv[1];
+  remove_id3v2(mp3filename);
+  remove_id3v1(mp3filename);
   return 0;
 }
 
